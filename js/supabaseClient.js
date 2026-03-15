3a4298828b48184e42c9321f8819ffafa6278c47
xav1221 <118613520+xav1221@users.noreply.github.com>
2026-03-15 20:01:04 +0100

    Fix formatting in supabaseClient.js
    modification url supabase

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://azdoqtfnwugnhhcchjfn.supabase.co";
+const SUPABASE_URL = "https://azdoqtfnwugnnhcchjfn.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_AmBB6eUCHMZU2jA0rHfUvA_FiWGSamF";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
